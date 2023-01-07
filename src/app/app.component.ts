d2388c6b77b2103f99a20da95e50fabf40f9f2a6
mersaucedo <mersaucedosantos@gmail.com>
2023-01-07 08:51:17 -0500

    sesión 13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  cities = ["Barcelona", "Madrid", "Lima"] ;
+  texto !: string; 
+  num2 !: number; 
   title = 'nueve';
-}
+  //fecha = new Date();
+  fecha = new Date("05/01/2023");
+  hoy = new Date("06/01/2023");
+  dieciseisDias = 1000 * 60 * 60 * 24 * 2;
+  //fechaM =new Date(this.hoy.getTime()+1000*60*60*24);
+  diferencia = this.hoy.getTime() -  this.fecha.getTime();
+  //Math.abs(this.hoy-this.fecha)
+  //horasTranscurridas = this.diferencia / 1000 / 60 / 60;
+  
+  //resta = this.fecha.getTime() - this.date.getTime();
+
+   
+  //datos;
+  // Seleccionamos o iniciamos el valor '0' del <select>
+  opcionSeleccionado = 0;
+  verSeleccion: string = '';
+   estado = [0,1,2,3,4];
+
+  constructor(){
+     // this.datos = ["suma","resta","multiplicacion", "division"];
+  }  
+
+  capturar() {
+      // Pasamos el valor seleccionado a la variable verSeleccion
+    
+
+}}
